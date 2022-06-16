3d2b8f5bafee4c7d9b0a2eb8d88fc7187926936a
jelena3214 <37124826+jelena3214@users.noreply.github.com>
2022-06-17 01:38:29 +0200

    novo

--- POOP/src/olympicgames/OlympicGames.java
+++ POOP/src/olympicgames/OlympicGames.java
@@ -1,6 +1,11 @@
 package olympicgames;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class OlympicGames {
 	public static Long athletes, events;
@@ -12,8 +17,8 @@ public class OlympicGames {
 		athletes = initAthletes(new String("C:\\Users\\Lenovo\\source\\repos\\Proba\\Proba\\athletes.txt"));
 	}
 	
-	//type 1 - individual, 2 - team
-	//medaltype 1 - gold, 2- silver, 3 - bronze
+	//type 1 - individual, 0 - team -1 nista
+	//medaltype 0 - gold, 1- silver, 2 - bronze, 3-NA -1 nista
 	public native HashMap<String, Integer> numOfParticipants(Long athletes, Long events, String sport, int year, int type, int medaltype);
 	public native HashMap<Integer, Integer> numOfDisciplinesSeason(Long athletes, Long events, String sport, int startYear, int endYear, int type, int medaltype, String season);
 	public native HashMap<Integer, Integer> averageHeightSeason(Long athletes, Long events, String sport, int startYear, int endYear, int type, int medaltype, String season);
@@ -30,18 +35,24 @@ public class OlympicGames {
 		System.out.println(athletes);
 		System.out.println("Proslo");
 		
-		/*HashMap<String, Integer> res = ol.numOfParticipants(athletes, events, "Athletics", 2016, 0, 0);
+		HashMap<String, Integer> res = ol.numOfParticipants(athletes, events, "Athletics", 2016, -1, -1);
 		System.out.println("Mapiralo");
 		int num = 0;
-		res.entrySet().forEach(entry -> {
-		    System.out.println(entry.getKey() + " " + entry.getValue());
-		});
-		for(Integer value: res.values()) {
-			num += value;
-		}
-		System.out.println(num);*/
 		
-		HashMap<Integer, Integer> res = ol.averageWeightSeason(athletes, events, "", 1992, 2000, 0, 0, "Summer");
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(res.entrySet());
+		// sorting the list elements
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+		
+		for(Entry<String, Integer> entry: list) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		System.out.println(num);
+		
+		/*HashMap<Integer, Integer> res = ol.averageWeightSeason(athletes, events, "", 1992, 2000, 0, 0, "Summer");
 		System.out.println("Mapiralo");
 		int num = 0;
 		res.entrySet().forEach(entry -> {
@@ -54,7 +65,7 @@ public class OlympicGames {
 		
 		res1.entrySet().forEach(entry -> {
 		    System.out.println(entry.getKey() + " " + entry.getValue());
-		});
+		});*/
 	}
 	
 }
--- POOP/src/piechart/WindowApp.java
+++ POOP/src/piechart/WindowApp.java
@@ -1,4 +1,5 @@
 package piechart;
+
 import xygraph.XYGraph;
 import olympicgames.*;
 
@@ -20,148 +21,229 @@ import java.util.TreeMap;
 import javax.swing.*;
 
 public class WindowApp extends JFrame {
-	//dodaj u prozor delove za filter, gde se unosi i na dugme prikaz nek prikaze taj grafik
-	//removujemo svaki put panel i dodajemo ga
+	// dodaj u prozor delove za filter, gde se unosi i na dugme prikaz nek prikaze
+	// taj grafik
+	// removujemo svaki put panel i dodajemo ga
 	JRadioButton numOfAth, numOfDisc, avrgHeight, avrgWeight;
 	JButton show = new JButton("Prikazi");
 	ButtonGroup g1 = new ButtonGroup();
 	JTextField sport, year, type, medal, startY, endY;
 	JPanel center = new JPanel();
-	private boolean ready = true; //TODO vidi oco kako iskorititi
+	private boolean ready = true; // TODO vidi oco kako iskorititi
 	private String sportF;
 	private int yearF, typeF, medalF, endYear, startYear;
 	private OlympicGames ol = new OlympicGames();
 	private int choice;
 	private PieChart loans;
 	private XYGraph graph;
-	
+
 	public WindowApp() {
-		setBounds(400, 100,600,600);
+		setBounds(400, 100, 600, 600);
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				dispose();
 			}
 		});
-		
+
 		populateWindow();
 		addListeners();
-		
+
 		setResizable(false);
 		setVisible(true);
 	}
 
 	private void addListeners() {
-		numOfAth.addActionListener((ae)->{
+		numOfAth.addActionListener((ae) -> {
 			choice = 1;
 		});
-		
-		numOfDisc.addActionListener((ae)->{
+
+		numOfDisc.addActionListener((ae) -> {
 			choice = 2;
 		});
 		
-		show.addActionListener((ae)->{
+		avrgHeight.addActionListener((ae)->{
+			choice = 3;
+		});
+		
+		avrgWeight.addActionListener((ae)->{
+			choice = 4;
+		});
+
+		show.addActionListener((ae) -> {
 			sportF = sport.getText();
-			if(!year.getText().isEmpty()) {
+			if (!year.getText().isEmpty()) {
 				yearF = Integer.parseInt(year.getText());
-			}else yearF = 0;
-			if(!type.getText().isEmpty()) {
+			} else
+				yearF = 0;
+			if (!type.getText().isEmpty()) {
 				typeF = Integer.parseInt(type.getText());
-			}else typeF = 0;
-			if(!medal.getText().isEmpty()) {
+			} else
+				typeF = -1;
+			if (!medal.getText().isEmpty()) {
 				medalF = Integer.parseInt(medal.getText());
-			}else medalF = 0;
-			if(!startY.getText().isEmpty())startYear = Integer.parseInt(startY.getText());
-			if(!endY.getText().isEmpty())endYear = Integer.parseInt(endY.getText());
-			if(ready) {
-				switch(choice) {
+			} else
+				medalF = -1;
+			if (!startY.getText().isEmpty())
+				startYear = Integer.parseInt(startY.getText());
+			if (!endY.getText().isEmpty())
+				endYear = Integer.parseInt(endY.getText());
+			if (ready) {
+				switch (choice) {
 				case 1:
 					HashMap<String, Integer> res = ol.numOfParticipants(0L, 0L, sportF, yearF, typeF, medalF);
-					List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(res.entrySet());  
-					//sorting the list elements  
-					Collections.sort(list, new Comparator<Entry<String, Integer>>()   
-					{
-						public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2)
-						{  
+					List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(res.entrySet());
+					// sorting the list elements
+					Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+						public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
 							return o2.getValue().compareTo(o1.getValue());
 						}
-					});  
-					if(loans != null) {
+					});
+					if (loans != null) {
 						WindowApp.this.remove(loans);
 					}
-					
-					if(graph != null) {
+
+					if (graph != null) {
 						WindowApp.this.remove(graph);
 					}
-					
+
 					loans = new PieChart();
 					PieSlice other = new PieSlice(Color.BLUE, 0, "Other");
-					int iterator = 1;
-					for(Entry<String, Integer> entry: list) {
-						if(iterator > 8) {
-					    	other.addSize(entry.getValue());
-					    	continue;
-					    }
-					    iterator++;
-					    loans.addSlice(new PieSlice(new Color((int)(Math.random() * 0x1000000)), entry.getValue(), entry.getKey()));
+					int iterator = 0;
+					for (Entry<String, Integer> entry : list) {
+						if (iterator > 10) {
+							other.addSize(entry.getValue());
+							continue;
+						}
+						iterator++;
+						loans.addSlice(new PieSlice(new Color((int) (Math.random() * 0x1000000)), entry.getValue(),
+								entry.getKey()));
 					}
 
 					loans.addSlice(other);
-					//center.add(loans);
-					//center.revalidate();
+					// center.add(loans);
+					// center.revalidate();
 					WindowApp.this.add(loans, BorderLayout.CENTER);
 					loans.revalidate();
 					loans.paint(getGraphics());
 					validate();
 					repaint();
 					break;
-				case 2: 
-					HashMap<Integer, Integer> res0 = ol.numOfDisciplinesSeason(0L, 0L, sportF, startYear, endYear, 0, 0, "Summer");
+				case 2:
+					HashMap<Integer, Integer> res0 = ol.numOfDisciplinesSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Summer");
 
-					HashMap<Integer, Integer> res1 = ol.numOfDisciplinesSeason(0L, 0L, sportF, startYear, endYear, 0, 0, "Winter");
+					HashMap<Integer, Integer> res1 = ol.numOfDisciplinesSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Winter");
 
-					List<Entry<Integer, Integer>> list0 = new LinkedList<Entry<Integer, Integer>>(res0.entrySet());  
-					Collections.sort(list0, new Comparator<Entry<Integer, Integer>>()   
-					{
-						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2)
-						{  
+					List<Entry<Integer, Integer>> list0 = new LinkedList<Entry<Integer, Integer>>(res0.entrySet());
+					Collections.sort(list0, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
 							return o2.getValue().compareTo(o1.getValue());
 						}
-					}); 
-					List<Entry<Integer, Integer>> list1 = new LinkedList<Entry<Integer, Integer>>(res1.entrySet());  
-					Collections.sort(list1, new Comparator<Entry<Integer, Integer>>()   
-					{
-						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2)
-						{  
+					});
+					List<Entry<Integer, Integer>> list1 = new LinkedList<Entry<Integer, Integer>>(res1.entrySet());
+					Collections.sort(list1, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
 							return o2.getValue().compareTo(o1.getValue());
 						}
-					}); 
+					});
 
-					if(loans != null) {
+					if (loans != null) {
 						WindowApp.this.remove(loans);
 					}
-					if(graph != null) {
+					if (graph != null) {
 						WindowApp.this.remove(graph);
 					}
-					
+
 					graph = new XYGraph(list0, list1, 400, 400, startYear - 1, endYear);
-					
-					
+
 					WindowApp.this.add(graph, BorderLayout.CENTER);
 					graph.revalidate();
 					graph.paint(getGraphics());
 					validate();
 					repaint();
+
+					break;
 					
+				case 3:
+					HashMap<Integer, Integer> res2 = ol.averageHeightSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Summer");
+
+					HashMap<Integer, Integer> res3 = ol.averageHeightSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Winter");
+
+					List<Entry<Integer, Integer>> list2 = new LinkedList<Entry<Integer, Integer>>(res2.entrySet());
+					Collections.sort(list2, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+							return o2.getValue().compareTo(o1.getValue());
+						}
+					});
+					List<Entry<Integer, Integer>> list3 = new LinkedList<Entry<Integer, Integer>>(res3.entrySet());
+					Collections.sort(list3, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+							return o2.getValue().compareTo(o1.getValue());
+						}
+					});
+
+					if (loans != null) {
+						WindowApp.this.remove(loans);
+					}
+					if (graph != null) {
+						WindowApp.this.remove(graph);
+					}
+
+					graph = new XYGraph(list2, list3, 400, 400, startYear - 1, endYear);
+
+					WindowApp.this.add(graph, BorderLayout.CENTER);
+					graph.revalidate();
+					graph.paint(getGraphics());
+					validate();
+					repaint();
+					break;
+				case 4:
+					HashMap<Integer, Integer> res4 = ol.averageWeightSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Summer");
+
+					HashMap<Integer, Integer> res5 = ol.averageWeightSeason(0L, 0L, sportF, startYear, endYear, 0, 0,
+							"Winter");
+
+					List<Entry<Integer, Integer>> list4 = new LinkedList<Entry<Integer, Integer>>(res4.entrySet());
+					Collections.sort(list4, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+							return o2.getValue().compareTo(o1.getValue());
+						}
+					});
+					List<Entry<Integer, Integer>> list5 = new LinkedList<Entry<Integer, Integer>>(res5.entrySet());
+					Collections.sort(list5, new Comparator<Entry<Integer, Integer>>() {
+						public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+							return o2.getValue().compareTo(o1.getValue());
+						}
+					});
+
+					if (loans != null) {
+						WindowApp.this.remove(loans);
+					}
+					if (graph != null) {
+						WindowApp.this.remove(graph);
+					}
+
+					graph = new XYGraph(list4, list5, 400, 400, startYear - 1, endYear);
+
+					WindowApp.this.add(graph, BorderLayout.CENTER);
+					graph.revalidate();
+					graph.paint(getGraphics());
+					validate();
+					repaint();
 					break;
 				}
 			}
 		});
-		
+
 	}
 
 	private void populateWindow() {
-		JPanel left = new JPanel(new GridLayout(0,1));
+		JPanel left = new JPanel(new GridLayout(0, 1));
 		numOfAth = new JRadioButton("Broj ucesnika");
 		numOfDisc = new JRadioButton("Broj disciplina");
 		avrgHeight = new JRadioButton("Prosecna visina");
@@ -175,15 +257,15 @@ public class WindowApp extends JFrame {
 		left.add(avrgHeight);
 		left.add(avrgWeight);
 		add(left, BorderLayout.WEST);
-		
+
 		JPanel bottom = new JPanel(new GridLayout(2, 0));
 		sport = new JTextField();
 		year = new JTextField();
-		type  = new JTextField();
+		type = new JTextField();
 		medal = new JTextField();
 		startY = new JTextField();
 		endY = new JTextField();
-		
+
 		bottom.add(new Label("Sport:"));
 		bottom.add(sport);
 		bottom.add(new Label("Year:"));
@@ -203,8 +285,8 @@ public class WindowApp extends JFrame {
 		allBottom.add(help);
 		add(allBottom, BorderLayout.SOUTH);
 		center.setBackground(Color.white);
-		//center.add(new Label("Prazno"));
-		//add(center, BorderLayout.CENTER);
+		// center.add(new Label("Prazno"));
+		// add(center, BorderLayout.CENTER);
 	}
 
 	public static void main(String[] args) {
@@ -213,58 +295,41 @@ public class WindowApp extends JFrame {
 
 }
 
-
-
-/*OlympicGames ol = new OlympicGames();
-HashMap<String, Integer> res = ol.numOfParticipants(0L, 0L, "Athletics", 2016, 0, 1);
-
-
-List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(res.entrySet());  
-//sorting the list elements  
-Collections.sort(list, new Comparator<Entry<String, Integer>>()   
-{
-	public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2)
-	{  
-		return o2.getValue().compareTo(o1.getValue());
-	}
-});  
-
-PieChart loans = new PieChart();
-PieSlice other = new PieSlice(Color.BLUE, 0, "Other");
-int iterator = 1;
-for(Entry<String, Integer> entry: list) {
-	if(iterator > 8) {
-    	other.addSize(entry.getValue());
-    	continue;
-    }
-    iterator++;
-    loans.addSlice(new PieSlice(new Color((int)(Math.random() * 0x1000000)), entry.getValue(), entry.getKey()));
-}
-
-loans.addSlice(other);
-this.add(loans);
-OlympicGames ol = new OlympicGames();
-HashMap<Integer, Integer> res = ol.averageHeightSeason(0L, 0L, "Ice Hockey", 1900, 2016, 0, 0, "Summer");
-
-HashMap<Integer, Integer> res1 = ol.averageHeightSeason(0L, 0L, "Ice Hockey", 1900, 2016, 0, 0, "Winter");
-
-List<Entry<Integer, Integer>> list = new LinkedList<Entry<Integer, Integer>>(res.entrySet());  
-Collections.sort(list, new Comparator<Entry<Integer, Integer>>()   
-{
-	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2)
-	{  
-		return o2.getValue().compareTo(o1.getValue());
-	}
-}); 
-
-List<Entry<Integer, Integer>> list1 = new LinkedList<Entry<Integer, Integer>>(res1.entrySet());  
-Collections.sort(list, new Comparator<Entry<Integer, Integer>>()   
-{
-	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2)
-	{  
-		return o2.getValue().compareTo(o1.getValue());
-	}
-}); 
-
-XYGraph a = new XYGraph(list, list1, 400, 400, 1899, 2016);
-add(a);*/
+/*
+ * OlympicGames ol = new OlympicGames(); HashMap<String, Integer> res =
+ * ol.numOfParticipants(0L, 0L, "Athletics", 2016, 0, 1);
+ * 
+ * 
+ * List<Entry<String, Integer>> list = new LinkedList<Entry<String,
+ * Integer>>(res.entrySet()); //sorting the list elements Collections.sort(list,
+ * new Comparator<Entry<String, Integer>>() { public int compare(Entry<String,
+ * Integer> o1, Entry<String, Integer> o2) { return
+ * o2.getValue().compareTo(o1.getValue()); } });
+ * 
+ * PieChart loans = new PieChart(); PieSlice other = new PieSlice(Color.BLUE, 0,
+ * "Other"); int iterator = 1; for(Entry<String, Integer> entry: list) {
+ * if(iterator > 8) { other.addSize(entry.getValue()); continue; } iterator++;
+ * loans.addSlice(new PieSlice(new Color((int)(Math.random() * 0x1000000)),
+ * entry.getValue(), entry.getKey())); }
+ * 
+ * loans.addSlice(other); this.add(loans); OlympicGames ol = new OlympicGames();
+ * HashMap<Integer, Integer> res = ol.averageHeightSeason(0L, 0L, "Ice Hockey",
+ * 1900, 2016, 0, 0, "Summer");
+ * 
+ * HashMap<Integer, Integer> res1 = ol.averageHeightSeason(0L, 0L, "Ice Hockey",
+ * 1900, 2016, 0, 0, "Winter");
+ * 
+ * List<Entry<Integer, Integer>> list = new LinkedList<Entry<Integer,
+ * Integer>>(res.entrySet()); Collections.sort(list, new
+ * Comparator<Entry<Integer, Integer>>() { public int compare(Entry<Integer,
+ * Integer> o1, Entry<Integer, Integer> o2) { return
+ * o2.getValue().compareTo(o1.getValue()); } });
+ * 
+ * List<Entry<Integer, Integer>> list1 = new LinkedList<Entry<Integer,
+ * Integer>>(res1.entrySet()); Collections.sort(list, new
+ * Comparator<Entry<Integer, Integer>>() { public int compare(Entry<Integer,
+ * Integer> o1, Entry<Integer, Integer> o2) { return
+ * o2.getValue().compareTo(o1.getValue()); } });
+ * 
+ * XYGraph a = new XYGraph(list, list1, 400, 400, 1899, 2016); add(a);
+ */
